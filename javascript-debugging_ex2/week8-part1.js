db817c11f604a41ecb12e56cbd262b399006a819
Amber Osborne <OsborneAP@cardiff.ac.uk>
2021-01-13 17:52:20 +0000

    adding event listeners in the nav bar

--- javascript-debugging_ex2/week8-part1.js
+++ javascript-debugging_ex2/week8-part1.js
@@ -1,1 +1,15 @@
 console.log("it works!");
+
+let nav_element = document.getElementsByTagName("nav")[0];
+
+nav_element.addEventListener("click", function () {
+    console.log("nav clicked");
+});
+
+let list_items = document.getElementsByTagName("li");
+
+for(var i = 0; i < list_items.length; i++) {
+    list_items[i].addEventListener("click", function () {
+        console.log("list item clicked");
+    })
+}
