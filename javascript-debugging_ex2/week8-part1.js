73a66a58bf3657a9c34ea1083e7f4728c385814b
Amber Osborne <OsborneAP@cardiff.ac.uk>
2021-01-19 16:41:21 +0000

    adding hover event listeners

--- javascript-debugging_ex2/week8-part1.js
+++ javascript-debugging_ex2/week8-part1.js
@@ -12,4 +12,18 @@ for(let i = 0; i < list_items.length; i++) {
     list_items[i].addEventListener("click", function() {
         console.log("list item " + i + " clicked");
     })
+};
+
+
+let changeClass = function(){
+    if (this.className === 'black') {
+        this.className = 'purple';
+    } else if (this.className === 'purple') {
+        this.className = 'black';
+    }
 }
+
+let header = document.getElementById('main-heading');
+console.log(header);
+header.addEventListener('mouseenter', changeClass);
+header.addEventListener('mouseout', changeClass);
