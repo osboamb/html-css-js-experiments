487abe714da5a6684c4835c92204b06b8f7f2078
Amber Osborne <OsborneAP@cardiff.ac.uk>
2021-01-19 16:06:38 +0000

    adding further event listeners and functions

--- javascript-debugging_ex2/week8-part1.js
+++ javascript-debugging_ex2/week8-part1.js
@@ -1,15 +1,15 @@
 console.log("it works!");
 
-let nav_element = document.getElementsByTagName("nav")[0];
+let nav_element = document.getElementsByTagName("nav")[0]; //[0] is the first tag (in this case first Nav)
 
-nav_element.addEventListener("click", function () {
+nav_element.addEventListener("click", function() {
     console.log("nav clicked");
-});
+}, useCapture=true);
 
 let list_items = document.getElementsByTagName("li");
 
-for(var i = 0; i < list_items.length; i++) {
-    list_items[i].addEventListener("click", function () {
-        console.log("list item clicked");
+for(let i = 0; i < list_items.length; i++) {
+    list_items[i].addEventListener("click", function() {
+        console.log("list item " + i + " clicked");
     })
 }
